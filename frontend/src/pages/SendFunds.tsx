55953d99d8fe961a529866a384c82546e799a862
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2023-07-30 18:16:32 +0100

    started working on the sendFunds

--- frontend/src/pages/SendFunds.tsx
+++ frontend/src/pages/SendFunds.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import ComponentHeader from "../components/componentsHeader/ComponentHeader";
 
 const SendFunds = () => {
-  return <div>SendFunds</div>;
+  const [paymentId, setPaymentId] = useState("");
+  const [amount, setAmount] = useState("");
+
+  return (
+    <div className="sendFunds">
+      <ComponentHeader />
+      <form className="form">
+        <input
+          value={paymentId}
+          onChange={(e) => setPaymentId(e.target.value)}
+          className="paymentId"
+          type={"text"}
+          placeholder="Payment Id"
+        />
+
+        <div className="password-container">
+          <input
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="amount"
+            placeholder="Amount"
+          />
+        </div>
+
+        <button className="button">Continue</button>
+      </form>
+    </div>
+  );
 };
 
 export default SendFunds;
