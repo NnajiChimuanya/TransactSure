f05cb1cf67f3dd7477ec06724fda1432dc67d6ef
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2023-08-02 17:26:21 +0100

    completed styling of the modal component

--- frontend/src/components/Modal/Modal.tsx
+++ frontend/src/components/Modal/Modal.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { MdClear } from "react-icons/md";
 
 const Modal = () => {
   return (
     <div className="modalContainer">
-      <div className="modal">Modal</div>
+      <div className="modal">
+        <div className="clearIcon">
+          <MdClear />
+        </div>
+
+        <div className="modalText">
+        Sorry, this PIN is not correct, please try again. 
+        </div>
+
+        <button className="modalButton">Try again</button>
+      </div>
     </div>
   );
 };
