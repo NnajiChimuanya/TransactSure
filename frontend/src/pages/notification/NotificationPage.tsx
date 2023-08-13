90d6b0a76242ecb5621ca8a0ac03135aaae0f712
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2023-08-13 13:51:19 +0100

    added the component header to the notifications page

--- frontend/src/pages/notification/NotificationPage.tsx
+++ frontend/src/pages/notification/NotificationPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Md10K } from "react-icons/md";
 import { NoNotification } from "../../svg";
+import ComponentHeader from "../../components/componentsHeader/ComponentHeader";
 
 const NotificationPage: React.FC = () => {
   return (
     <div>
+      <ComponentHeader />
       <NoNotification />
     </div>
   );
